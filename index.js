a5fea2bbfe7c3add9ff4f5346a375becefa63c75
jose <jruizzapata38@gmail.com>
2025-08-29 00:43:12 -0500

    Creacion de la funcion factorial

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ function dividir(num1, num2) {
   return num1 / num2;
 }
 
+function factorial(n) {
+  if (n < 0) return "Error: no existe factorial de negativos";
+  if (n === 0 || n === 1) return 1;
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
 // Función principal del programa
 function calculadora() {
   var opcion = prompt(
